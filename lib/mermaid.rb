2357c378be96dd564acedcc6cf598934607184c4
Jack Sweeney <jackcsweeney@gmail.com>
2023-10-29 09:56:14 -0700

    Add additional notes to mermaid.rb

--- lib/mermaid.rb
+++ lib/mermaid.rb
@@ -16,4 +16,6 @@ end
 # This code passes the test, however, I don't think this is the intended way for it to do so.
 # Both the age and get_older method merely set the age to a specific integer rather than make it
 # a dynamic property like name. Though in the test, it doesn't seem like the age is supposed to be
-# a dynamic property based on the fact it's not set as an argument anywhere, just listed as == 22
+# a dynamic property based on the fact it's not set as an argument anywhere, just listed as == 22.
+# Perhaps that was the intention and I was over complicating, but it seems like code that wouldn't
+# be useful.
